1c5de53afb18d42f8191c7900f495302db254d5f
Krzysztof Nowacki <krzysztof.nowacki06@gmail.com>
2019-08-08 18:21:29 +0200

    Initial Commit

---
+++ Invoice Maker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Invoice Maker
+//
+//  Created by Krzysiu on 08/08/2019.
+//  Copyright © 2019 k.now. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
